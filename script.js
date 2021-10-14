66b003b53af8c82eb9118f5656a9bac2abe905bf
Luis-Paulo-Silva-Reis <lpreis2010@gmail.com>
2021-10-14 09:41:52 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('hello!')
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+      console.log('ip');
+    }
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      // Descendo
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      // Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
